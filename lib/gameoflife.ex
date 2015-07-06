69599c9c5208007b0230a2f9ee07a3770dd4640c
simonewebdesign <hello@simonewebdesign.it>
2015-07-06 21:23:18 +0100

    :boom: Start

--- lib/gameoflife.ex
+++ lib/gameoflife.ex
@@ -1,2 +1,31 @@
-defmodule Gameoflife do
+defmodule Life do
+  @moduledoc """
+  # The Game of Life: Rules
+
+  ## 1st rule
+  Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+
+  ## 2nd rule
+  Any live cell with two or three live neighbours lives on to the next generation.
+
+  ## 3rd rule
+  Any live cell with more than three live neighbours dies, as if by overcrowding.
+
+  ## 4th rule
+  Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+  """
+
+  def render_cell do
+    IO.write "o"
+  end
+
+  def render_multiple_cells(num) when num <= 1 do
+    render_cell
+  end
+
+  def render_multiple_cells(num) do
+    render_cell
+    render_multiple_cells(num - 1)
+  end
+
 end
