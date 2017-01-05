e3c2045e3c8a60f6bdfe39eca9508a58600fdd24
Simone Vittori <hello@simonewebdesign.it>
2017-01-05 12:26:50 +0000

    Add more tests

--- test/life_test.exs
+++ test/life_test.exs
@@ -1,5 +1,5 @@
 defmodule LifeTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
   doctest Life
 
   test "Life.tick generates the next grid" do
@@ -10,7 +10,33 @@ defmodule LifeTest do
 
     expected =
       [{0, -1}, {0, 0}, {0, 1}, {1, -1}, {1, 0}, {1, 1}, {2, 0}, {2, 1}]
+      |> MapSet.new
 
-    assert MapSet.equal?(next_grid, MapSet.new(expected))
+    assert MapSet.equal?(next_grid, expected)
   end
+
+
+  test "alive cell with 2 neighbours lives on to the next generation" do
+    alive_cells = [{0, 0}, {1, 0}, {2, 0}] |> MapSet.new
+    expected_alive_cell = {1, 0}
+    assert Life.tick(alive_cells) |> MapSet.member?(expected_alive_cell)
+  end
+
+
+  test "dead cell with three live neighbours becomes a live cell" do
+    alive_cells =
+      [{0, 0}, {1, 0}, {2, 0}, {1, 1}]
+      |> MapSet.new
+
+    born_cells =
+      alive_cells
+      |> Life.tick
+
+    expected_born_cells =
+      [{1, -1}, {0, 1}, {2, 1}]
+      |> MapSet.new
+
+    assert born_cells == MapSet.union(alive_cells, expected_born_cells)
+  end
+
 end
