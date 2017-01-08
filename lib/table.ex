40cb9234241ba48300916014e899bd3d7b6ff3d0
Simone Vittori <hello@simonewebdesign.it>
2017-01-08 15:27:47 +0000

    Implement footer

--- lib/table.ex
+++ lib/table.ex
@@ -3,31 +3,42 @@ defmodule Table do
 
   @top_left "╔"
   @top_right "╗"
-  # @bottom_left "╚"
-  # @bottom_right "╝"
-  @horizontal "═" # repeat x3
+  @bottom_left "╚"
+  @bottom_right "╝"
+  @horizontal "═"
   # @vertical "║"
   @union_top "╦"
   # @union_left "╠"
   # @union_right "╣"
-  # @union_bottom "╩"
+  @union_bottom "╩"
 
   def print() do
     :ok
   end
 
   def header(size) do
-    @top_left <> do_header("", 3)
+    @top_left <> do_header("", size)
   end
   def do_header(str, n) when n <= 1 do
-    str <> duplicate(@horizontal, 3) <> @top_right
+    str <> line() <> @top_right
   end
   def do_header(str, n) do
-    s = str <> duplicate(@horizontal, 3) <> @union_top
+    s = str <> line() <> @union_top
     do_header(s, n - 1)
   end
 
+
   def footer(size) do
-    "╚═══╩═══╩═══╝"
+    @bottom_left <> do_footer("", size)
   end
+  def do_footer(str, n) when n <= 1 do
+    str <> line() <> @bottom_right
+  end
+  def do_footer(str, n) do
+    s = str <> line() <> @union_bottom
+    do_footer(s, n - 1)
+  end
+
+
+  defp line, do: duplicate(@horizontal, 3)
 end
