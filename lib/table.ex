0e5e1f93fb2dc9f096e90979ca58fa2fe3b19abd
Simone Vittori <hello@simonewebdesign.it>
2017-01-08 01:09:49 +0000

    Implement header

--- lib/table.ex
+++ lib/table.ex
@@ -1,11 +1,13 @@
 defmodule Table do
-  # @top_left "╔"
-  # @top_right "╗"
+  import String, only: [duplicate: 2]
+
+  @top_left "╔"
+  @top_right "╗"
   # @bottom_left "╚"
   # @bottom_right "╝"
-  # @horizontal "═" # repeat x3
+  @horizontal "═" # repeat x3
   # @vertical "║"
-  # @union_top "╦"
+  @union_top "╦"
   # @union_left "╠"
   # @union_right "╣"
   # @union_bottom "╩"
@@ -15,7 +17,14 @@ defmodule Table do
   end
 
   def header(size) do
-    "╔═══╦═══╦═══╗"
+    @top_left <> do_header("", 3)
+  end
+  def do_header(str, n) when n <= 1 do
+    str <> duplicate(@horizontal, 3) <> @top_right
+  end
+  def do_header(str, n) do
+    s = str <> duplicate(@horizontal, 3) <> @union_top
+    do_header(s, n - 1)
   end
 
   def footer(size) do
